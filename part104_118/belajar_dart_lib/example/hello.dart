1fe62b0c0f94dfdaf183c29435a3dd82bdf79d5c
Rama Aryasuta Weihan <75559653+Ramaarya@users.noreply.github.com>
2022-07-12 12:21:19 +0700

    Add files via upload

---
+++ part104_118/belajar_dart_lib/example/hello.dart
@@ -0,0 +1,11 @@
+import 'package:belajar_dart_lib/heloo.dart';
+
+void main() {
+  sayHello('Budi');
+
+  var customer = Customer();
+
+  var product = Product();
+
+  var category = Category();
+}
---
+++ part104_118/belajar_dart_lib/lib/belajar_dart_lib.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library belajar_dart_lib;
+
+export 'src/belajar_dart_lib_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
